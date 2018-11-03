9a8af3587dff185f5c2ec71c4f7832f20b86733b
Spike Burton <ralphburton3@gmail.com>
2018-11-03 17:25:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(whisper(string))
 }
+
+function sayHiToGrandma(string) {
+  if(string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+}
